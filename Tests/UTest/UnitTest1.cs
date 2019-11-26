71ddc3b21d65ef410038bbd08dcde4f136f8f6fa
Jannes Coetzee <jannes@k2.com>
2019-11-26 14:28:03 +0200

    WebForms_Owin_TestApp.UTests

---
+++ Tests/UTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebForms_Owin_TestApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
